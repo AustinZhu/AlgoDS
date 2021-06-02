f5709018891c72a5f2b3995f2b7f3b234b8061dd
AustinZhu <42071208+AustinZhu@users.noreply.github.com>
2021-06-03 04:46:29 +0900

    feat: add stack and queue

--- src/main/java/io/austinzhu/algo/structure/list/Queue.java
+++ src/main/java/io/austinzhu/algo/structure/list/Queue.java
@@ -1,4 +1,40 @@
 package io.austinzhu.algo.structure.list;
 
-public class Queue {
+import io.austinzhu.algo.exception.IndexOutOfBoundsException;
+import io.austinzhu.algo.interfaces.Algorithm;
+import io.austinzhu.algo.interfaces.Interactable;
+
+public class Queue<T extends Comparable<T>> implements Interactable<T> {
+
+    private LinkedList<T> list;
+
+    public Queue(LinkedList<T> list) {
+        this.list = list;
+    }
+
+    public static Queue<Integer> init(int size, int bound) {
+        return new Queue<>(LinkedList.init(size, bound));
+    }
+
+    @Algorithm
+    @Override
+    public void append(T element) throws IndexOutOfBoundsException {
+        list.append(element);
+    }
+
+    @Algorithm
+    @Override
+    public void eject() throws IndexOutOfBoundsException {
+        list.delete(0);
+    }
+
+    @Override
+    public void init(T... elements) {
+
+    }
+
+    @Override
+    public String toString() {
+        return list.toString();
+    }
 }
--- src/main/java/io/austinzhu/algo/structure/list/Stack.java
+++ src/main/java/io/austinzhu/algo/structure/list/Stack.java
@@ -1,5 +1,40 @@
 package io.austinzhu.algo.structure.list;
 
-public class Stack {
+import io.austinzhu.algo.exception.IndexOutOfBoundsException;
+import io.austinzhu.algo.interfaces.Algorithm;
+import io.austinzhu.algo.interfaces.Interactable;
 
+public class Stack<T extends Comparable<T>> implements Interactable<T> {
+
+    private final LinkedList<T> list;
+
+    public Stack(LinkedList<T> list) {
+        this.list = list;
+    }
+
+    public static Stack<Integer> init(int size, int bound) {
+        return new Stack<>(LinkedList.init(size, bound));
+    }
+
+    @Algorithm
+    @Override
+    public void append(T element) throws IndexOutOfBoundsException {
+        list.append(element);
+    }
+
+    @Algorithm
+    @Override
+    public void eject() throws IndexOutOfBoundsException {
+        list.eject();
+    }
+
+    @Override
+    public void init(T... elements) {
+
+    }
+
+    @Override
+    public String toString() {
+        return list.toString();
+    }
 }
